7a1e0b8150e4447596c342f3e03ae79bb55a20c6
Nasif-Hasan <nasifhasan35@gmail.com>
2022-09-02 23:57:31 +0600

    menu set

--- js/app.js
+++ js/app.js
@@ -7,8 +7,23 @@ const loadAllNews = async() => {
 
 const catagoriMenu = async() => {
     // console.log(loadAllNews());
-    const data = await loadAllNews()
-    console.log(data.data.news_category)
+    const data = await loadAllNews();
+    const newsData = data.data.news_category
+    // console.log(newsData);
+
+    const catagoryMenu = document.getElementById('catagory-menu') 
+
+    for (const news of newsData){
+        // console.log(news.category_name);
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a class="px-8 text-lg">${news.category_name}</a>
+        `
+        catagoryMenu.appendChild(li)
+
+    }
+    
+    // console.log(data.data.news_category)
 }
 
 catagoriMenu()
